c850b88abbc163b3d3801de2aed480eb9e78b6d0
Ivanin Alexandr <ivanin.alexandr1994@yandex.ru>
2021-12-23 10:12:19 +0300

    create class Player

--- js/playersObj.js
+++ js/playersObj.js
@@ -1,36 +1,33 @@
-const player1 = {
-  player:1,
-  name:'Scorpion',
-  hp:100,
-  img:'http://reactmarathon-api.herokuapp.com/assets/scorpion.gif',
-  weapon:['Гарпун'],
-  changeHp,
-  elHP,
-  renderHP,
+class Player{
+  constructor(props){
+    const {player, name, hp, img} = props
+    this.player = player
+    this.name = name
+    this.hp = hp
+    this.img = img
+  }
+  changeHp = (hp) => {
+    this.hp -= hp
+  if(this.hp <= 0){ 
+    this.hp = 0
+  }
+  }
+  #elHP = () => document.querySelector(`.player${this.player} .life`)
+  renderHP = () => this.#elHP().style.width = `${this.hp}%`
 }
-const player2 = {
+
+const player1 = new Player({
+  player:1,
+    name:'Scorpion',
+    hp:100,
+    img:'http://reactmarathon-api.herokuapp.com/assets/scorpion.gif',
+})
+const player2 = new Player({
   player:2,
   name:'Kitana',
   hp:100,
   img:'http://reactmarathon-api.herokuapp.com/assets/kitana.gif',
-  weapon:['Веера'],
-  changeHp,
-  elHP,
-  renderHP,
-}
-function changeHp(hp){
-  this.hp -= hp
-  if(this.hp <= 0){ 
-    this.hp = 0
-  }
-}
-function elHP(){
-return document.querySelector(`.player${this.player} .life`)
-}
-
-function renderHP(){
-this.elHP().style.width = `${this.hp}%`
-}
+})
 
 export {
   player1,
